ef98e5858f49314795fa27d1410f681f24b2116e
Johnny <jgarciga@asu.edu>
2026-02-16 19:45:12 -0700

    Remove build files and ignore them

--- SoundManager/Builds/LinuxMakefile/build/intermediate/Debug/include_juce_core_CompilationTime_9257742c.d
+++
@@ -1,3 +0,0 @@
-build/intermediate/Debug/include_juce_core_CompilationTime_9257742c.o: \
- ../../JuceLibraryCode/include_juce_core_CompilationTime.cpp \
- /home/joh2381113/JUCE/modules/juce_core/juce_core_CompilationTime.cpp
